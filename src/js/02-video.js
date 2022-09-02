a1744ac646d4ca3237d6faa03cbef77abd9c1c66
vitalii-khymchuk <vitalii.workbox001@gmail.com>
2022-09-02 22:45:37 +0300

    task02_done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+readCurrentTime();
+
+function readCurrentTime() {
+  const time = localStorage.getItem('videoPlayerTime');
+  if (time) {
+    setCurrentTime(time);
+  }
+}
+
+function setCurrentTime(time) {
+  player.setCurrentTime(time);
+}
+
+function getCurrentTime() {
+  player.getCurrentTime().then(saveCurrentTime);
+}
+
+function saveCurrentTime(time) {
+  localStorage.setItem('videoPlayerTime', time);
+}
